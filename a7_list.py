ce9a07388b9159506333f896092cc9b9e3beac36
tusharviradiya <tusharviradiya2@gmail.com>
2024-04-30 18:51:48 +0530

    list topic added

---
+++ a7_list.py
@@ -0,0 +1,34 @@
+# list
+# list is class
+# when any language is oop so there is encapsulation topic is availabele.
+# list is iterable
+# list is mutable
+# in list all type of integer
+l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+# append and insert
+l1.append(11)
+l1.insert(0, 0)
+print(l1)
+
+# packing and unpacking
+
+# list() method
+# len()
+# min()
+# max()
+# sum()
+# sorted() always return list
+
+# comprehension operator in list
+# concatenation operator in list
+# repetition operator in list
+
+# list of list
+l3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+print(l3[0][1]);
+l3.pop(0);
+
+# list comprehension
+[i*2 for i in range(10)]
+
+# mutability and hashability
