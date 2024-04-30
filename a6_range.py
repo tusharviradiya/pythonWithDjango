a38a1bdc3bcc8e2727aae218aff039208788642b
tusharviradiya <tusharviradiya2@gmail.com>
2024-04-30 16:32:30 +0530

    file updation

---
+++ a6_range.py
@@ -0,0 +1,22 @@
+# iterators
+
+# range
+# range is class
+# range is iterable
+# range is immutable
+# in range all type of integer
+# range(start, stop, step)step means comman differance
+x = range(1, 10, 2);
+for i in x:
+    print(i);
+# range(end)
+# range(start, end)
+
+# list
+# tuple
+# str
+# byte
+# bytearray
+# set
+# frozenset
+# dict
