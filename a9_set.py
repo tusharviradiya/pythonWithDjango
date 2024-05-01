9fe56810f0f1620ef8e1e8ad7b5afd33499a04bd
tusharviradiya <tusharviradiya2@gmail.com>
2024-05-01 12:37:19 +0530

    set topic is added

---
+++ a9_set.py
@@ -0,0 +1,14 @@
+# set
+# set is class
+# set is iterable
+# set is mutable
+# set is a not sequence
+# slicing operator is not allowed
+
+# how to create set
+s1 = {"hello", "tushar", "viradya"}
+s2 = set([1, 2, 3, 4, 5])
+s3 = {}
+s4 = set()
+
+# concatenation and repetition are not allowed in set
