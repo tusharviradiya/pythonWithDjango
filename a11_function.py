5c16b57daa9bd60f4ecaacfc7bad0490abab1758
tusharviradiya <tusharviradiya2@gmail.com>
2024-05-01 14:22:42 +0530

    function topic added

---
+++ a11_function.py
@@ -0,0 +1,20 @@
+# take nothing return nothing
+def hello():
+    print("hello world")
+hello()
+
+# take nothing return something
+def greet():
+    return "hello world"
+greet()
+
+# take something return something
+def add(x, y):
+    return x + y
+add(1, 2)
+
+# take something return nothing
+def print_two(a, b):
+    print(a)
+    print(b)
+print_two(1, 2)
