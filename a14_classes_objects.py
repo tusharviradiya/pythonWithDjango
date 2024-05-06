7d0149c8f7dce47d6e0ed202555ce1ddfed198f6
tusharviradiya <tusharviradiya2@gmail.com>
2024-05-06 11:35:00 +0530

    classes and object

---
+++ a14_classes_objects.py
@@ -0,0 +1,24 @@
+# comman noun and prper noun
+# in python class is comman noun and object is proper noun
+# in python data hiding concept is not used
+# attributes in python called variables
+# how to create class
+class Firstclass :
+    x = 10
+    def student():
+        print("hello")
+
+t1 = Firstclass()
+# instunce object are empty at creation time
+
+# __init__() method
+# this is constructor
+# this is a method
+# this is a special method
+# this is called when object is created
+
+# type of varible
+# local variable
+# global variable
+# instance object variable
+# class object variable
