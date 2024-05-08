caa625bc8030710c45729c40300eb6cf7d6f9d37
tusharviradiya <tusharviradiya2@gmail.com>
2024-05-08 12:15:36 +0530

    practice some question on condition and function

--- a11_function.py
+++ a11_function.py
@@ -1,20 +1,30 @@
 # take nothing return nothing
-def hello():
-    print("hello world")
-hello()
+# def hello():
+#     print("hello world")
+# hello()
 
 # take nothing return something
-def greet():
-    return "hello world"
-greet()
+# def greet():
+#     return "hello world"
+# greet()
 
 # take something return something
-def add(x, y):
-    return x + y
-add(1, 2)
+# def add(x, y):
+#     return x + y
+# add(1, 2)
 
 # take something return nothing
-def print_two(a, b):
-    print(a)
-    print(b)
-print_two(1, 2)
+# def print_two(a, b):
+#     print(a)
+#     print(b)
+# print_two(1, 2)
+
+# practice of function problem
+
+# question 1
+# return squre of a number
+def square(x):
+    return x * x
+print(square(5))
+
+print("tushar"*10)
---
+++ a17_condition_questions.py
@@ -0,0 +1,24 @@
+# question 1
+# discription : categorise given age
+# age = int(input("enter your age : "))
+# if age < 13:
+#     print("child")
+    
+# elif age >= 13 and age < 18:
+#     print("teenager")
+
+# else:
+#     print("adult")
+
+# question 2
+# discription : give discount on movie ticket
+age = int(input("enter your age : "))
+if age < 13:
+    print("child")
+    print("10% discount")  
+elif age >= 13 and age < 18:
+    print("teenager")
+    print("15% discount")
+else:
+    print("adult")
+    print("20% discount")
