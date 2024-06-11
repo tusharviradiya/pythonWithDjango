7b774e2eb13aaec5bc97a44ee8da45583e2390f4
tusharviradiya <tusharviradiya2@gmail.com>
2024-06-11 17:32:04 +0530

    update setting file

--- revision of jango/firstproject/firstproject/settings.py
+++ revision of jango/firstproject/firstproject/settings.py
@@ -38,8 +38,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tailwind",
     "first_app",
+    "theme",
 ]
+
+TAILWIND_APP_NAME = "theme"
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
